4643092d6411676c879d8eb19068cefdff6fd15d
zhao.zhang <zhao.zhang@upai.com>
2021-09-30 09:18:44 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+<p align="center">
+  <a href="https://github.com/zq99299/repository-summary" target="_blank">
+     <img width="200" src="./docs/.vuepress/public/mlogo.svg" alt="logo">
+  </a>
+  <br/>
+  <br/>
+  <a href="https://github.com/zq99299/repository-summary" target="_blank"> 
+    所有笔记 → 系列导航
+  </a>
+</p>
+
+# note-combat
+
+实战笔记：主要定位是在工作中实际遇到的难题、没有接触过、或稍有难度的真实场景解决记录；
+
+这不同于其他的笔记，其他笔记是系统的学习某一个知识点。
+
+在线阅读：https://zq99299.github.io/note-combat/
+
+## 运行方式
+
+- 写作预览：
+
+  yarn docs:dev
+- 打包发布：
+
+  yarn docs:build
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,81 @@
+const springSc = require('../spring')
+const dockerSc = require('../docker')
+const gitlabSc = require('../gitlab')
+
+module.exports = {
+  // ~ 开发配置
+  dest: 'build/.vuepress/dist',  // 目录配置在外,纯粹是有代码洁癖和强迫症，并不能规避开发模式下同时构建不报错的问题
+  host: 'localhost', // dev 的域名
+  port: 8080,
+
+  // ~ 站点配置
+  title: '实战笔记',
+  description: '主要定位是在工作中实际遇到的难题、没有接触过、或稍有难度的真实场景解决记录',
+  base: '/note-combat/', // gh-page 中是增加了项目名
+  head:[
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+  ],
+  markdown: {
+    lineNumbers: true
+  },
+  themeConfig: {
+    docsDir: 'docs',
+    sidebar: 'auto',
+    sidebarDepth: 3, // 嵌套标题侧边栏提取深度，最大为 2，会提取到 h3
+    lastUpdated: '上次更新', // string | boolean
+    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+    repo: 'https://github.com/kougazhang/note-combat',
+    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+    repoLabel: 'GitHub',
+    docsDir: 'docs',
+    // 假如文档放在一个特定的分支下：
+    docsBranch: 'main',
+    // 默认是 false, 设置为 true 来启用
+    editLinks: true,
+    // 默认为 "Edit this page"
+    editLinkText: '帮助我们改善此页面！',
+    logo: '/mlogo.svg',
+    // 主题级别的配置
+    serviceWorker: {
+      updatePopup: true // Boolean | Object, 默认值是 undefined.
+    },
+    nav: [
+      { text: 'Spring', link: '/spring/' },
+      { text: 'Docker', link: '/docker/' },
+      { text: 'GitLab', link: '/gitlab/' }
+    ],
+    sidebar: {
+      '/spring/': springSc(),
+      '/docker/': dockerSc(),
+      '/gitlab/': gitlabSc()
+    }
+  },
+  plugins: [
+    ['@vuepress/back-to-top', true],
+    ['@vuepress/pwa', {
+      serviceWorker: true,
+      updatePopup: {
+        message: '有新内容更新啦~',
+        buttonText: '立即获取新内容，确定后稍后自动刷新'
+      }
+    }],
+    ['@vuepress/medium-zoom', {
+      selector: '.theme-default-content img'
+    }],
+    ['@vuepress/search', {
+      searchMaxSuggestions: 10
+    }],
+    ['vuepress-plugin-code-copy', true],
+    ['@vuepress/last-updated', {
+      transformer: (timestamp, lang) => {
+        // 不要忘了安装 moment
+        const moment = require('moment')
+        return moment(timestamp).format('YYYY-MM-DD HH:mm:ss')
+      }
+    }],
+    ['vuepress-plugin-tags'],
+    ['vuepress-plugin-baidu-autopush', true]
+  ]
+}
